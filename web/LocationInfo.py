8af020871fe4d4b62496be7cb6f69a91e3875594
ianlai2 <ian.lai.002@gmail.com>
2024-01-27 22:44:47 -0600

    includes backend files

---
+++ web/LocationInfo.py
@@ -0,0 +1,17 @@
+# implement LocationInfo class 
+# remove 'pass' keyword and replace with code
+class LocationInfo:
+    def __init__(self):
+        self.description = 'None'
+        self.funFact = 'None'
+
+    # TODO implement setters, getters, and checkers for description and funFact
+    # setters should update respective variable, 
+    # getters return respective variables as a string, 
+    # checkers are booleans and should return true if there is a value 
+
+    # TODO implement the function __str__ to output description and funFact as strings seperated by a newline
+    def __str__(self):
+        pass
+
+
