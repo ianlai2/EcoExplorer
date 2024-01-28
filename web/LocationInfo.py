6755d637d7df56d57eea63bff7eb0760fed3fa64
Ethanchooo <C0808614@tamu.edu>
2024-01-27 23:26:18 -0600

    Moved comments and commented out test cases

--- web/LocationInfo.py
+++ web/LocationInfo.py
@@ -6,18 +6,20 @@ class LocationInfo:
         self.funFact = funFact
 
  # TODO implement setters, getters, and checkers for description and funFact
+ # getters return respective variables as a string,
     def getdescription(self):
         return self.description
 
     def getfunfact(self):
         return self.funFact
-
+# setters should update respective variable, 
     def setfunfact(self, new_funFact):
         self.funFact = new_funFact
 
     def setdescription(self, value):
         self.description = value
 
+ # checkers are booleans and should return true if there is a value 
     def checkfact(self):
         if (self.funFact == 'None'):
             return False
@@ -30,25 +32,19 @@ class LocationInfo:
         else:
             return True
 
-
- # TODO implement setters, getters, and checkers for description and funFact
-    # setters should update respective variable, 
-    # getters return respective variables as a string, 
-    # checkers are booleans and should return true if there is a value 
-
     # TODO implement the function __str__ to output description and funFact as strings seperated by a newline
     def __str__(self):
         print(self.description + '\n')
         print(self.funFact)
         pass
 
-Houston = LocationInfo("harris_county")
-Houston.getdescription()
-Houston.getfunfact()
-Houston.setdescription("Ford Bend county")
-Houston.getdescription()
-Houston.getfunfact()
-Houston.__str__()
+# Houston = LocationInfo("harris_county")
+# Houston.getdescription()
+# Houston.getfunfact()
+# Houston.setdescription("Ford Bend county")
+# Houston.getdescription()
+# Houston.getfunfact()
+# Houston.__str__()
 
 
 
